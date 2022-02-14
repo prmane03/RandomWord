8ba08f5fd7c9094c8ed62e0dca16cd2caf52a43d
Pratik Mane <49906534+prmane03@users.noreply.github.com>
2022-02-14 13:02:07 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+ var word = document.getElementById("word")
+        var definition = document.getElementById("definition")
+        var pronunciation = document.getElementById("pronunciation")
+        var time = document.getElementById("time")
+        var img = document.getElementById("img")
+        var reset = document.getElementById("reset")
+
+        let speech = new SpeechSynthesisUtterance();
+
+        speech.lang = "en-US";
+        speech.text = pronunciation.value;
+        speech.volume = 1;
+        speech.rate = 1;
+        speech.pitch = 1;                
+
+
+        function speak(speech){
+
+            window.speechSynthesis.speak();
+
+        }
+
+        async function getWord() {
+            try {
+                let res = await fetch("https://random-words-api.vercel.app/word/");
+                var result = await res.json();
+               
+                word.innerHTML = result[0].word
+                pronunciation.innerHTML = result[0].pronunciation
+                definition.innerHTML = result[0].definition
+                img.src = "https://avatars.dicebear.com/api/identicon/"+result[0].word+".svg"
+                time.innerHTML = new Date().toLocaleString();
+                
+            }catch (error) {    
+                console.log(error);
+            }
+        }
+        getWord();
+
+        reset.addEventListener('click',()=>{
+            getWord();
+        })
